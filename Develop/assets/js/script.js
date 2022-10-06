43cde546eaa23744d2df3c6e05fd4d2049489138
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-10-06 12:51:35 -0400

    Added code to HTML and JS files (added current date on Jumbotron using moment)

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,3 +1,6 @@
 // Use moment to:
 // add current date on the jumbotron
 // format table rows to change color depending on past, present, future
+
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
